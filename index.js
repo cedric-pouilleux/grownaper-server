8afd5357411cb46633551f619685b0b9af1eccd4
Minart <mino.mnz@gmail.com>
2022-03-13 14:53:02 +0100

    Test with scoped client variable

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const { MongoClient } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.MONGO_DATABASE_CLUSTER}`;
 
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
 app.use(cors({credentials: true, origin: true}));
 app.options('*', cors());
 
@@ -18,6 +13,10 @@ app.options('*', cors());
  * Init route
  */
 app.get('/', (req, res) => {
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
     client.connect(async (err) => {
         const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
         const all = await collection.find({}).toArray();
