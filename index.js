a28f5ef65ae9dd6eec859dc654f7a1951bc8bba0
Minart <mino.mnz@gmail.com>
2022-03-13 15:13:36 +0100

    Hosting config : Scope all behavior

--- index.js
+++ index.js
@@ -4,35 +4,32 @@ const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.MONGO_DATABASE_CLUSTER}`;
-
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
 app.use(cors({credentials: true, origin: true}));
 app.options('*', cors());
 
 /**
  * Init route
  */
-app.get('/', (req, res) => {
-    client.connect(err => {
-        const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
-        collection
-            .find({})
-            .toArray((err, result) => {
-                if(result && !err){
-                    res.status(200).json({
-                        feeders : result
-                    });
-                } else {
+app.get('/', async (req, res) => {
+    const uri = `mongodb+srv://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.MONGO_DATABASE_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
 
-                    res.status(404);
-                }
-        });
+    const mongoClient = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
+
+    const client = await mongoClient.connect();
+    const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
+    const all = await collection.find({}).toArray();
+    await mongoClient.close();
+
+    if(all){
+        res.status(200).json({
+            feeders : all
+        });
+    } else {
+        res.status(404);
+    }
 });
 
 app.listen(process.env.PORT || 3000, function() {
