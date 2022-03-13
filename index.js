6a07eeddb7f227b2e4197141affb3ecabf980970
Minart <mino.mnz@gmail.com>
2022-03-13 14:21:42 +0100

    Configurate mongodb, use env, execut simple db read

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
-app.use(
-    cors({
-        credentials: true,
-        origin: true
-    })
-);
+const uri = `mongodb+srv://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.MONGO_DATABASE_CLUSTER}`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors({credentials: true, origin: true}));
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('Working!!!'));
+/**
+ * Init route
+ */
+app.get('/', (req, res) => {
+    client.connect(async (err) => {
+        const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
+        const all = await collection.find({}).toArray();
+        await client.close();
+        res.status(200).json({
+            feeders : all
+        });
+    });
+});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
