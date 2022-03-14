4eeb1204dc07ff5c72924a92aa7b7c5602f87935
Minart <mino.mnz@gmail.com>
2022-03-14 19:29:39 +0100

    Add nodemon for dev env

--- index.js
+++ index.js
@@ -7,30 +7,27 @@ const { MongoClient } = require('mongodb');
 app.use(cors({credentials: true, origin: true}));
 app.options('*', cors());
 
+const { MONGO_DATABASE_USERNAME, MONGO_DATABASE_PASSWORD, MONGO_DATABASE_CLUSTER, MONGO_DATABASE } = process.env;
+const uri = `mongodb+srv://${MONGO_DATABASE_USERNAME}:${MONGO_DATABASE_PASSWORD}@${MONGO_DATABASE_CLUSTER}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+
 /**
- * Init route
+ * Breaders CRUD
  */
-app.get('/', async (reqgi, res) => {
-    const uri = `mongodb+srv://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.MONGO_DATABASE_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
-
-    console.log(uri);
-
-    const mongoClient = new MongoClient(uri, {
+app.route('/breeders')
+    .get(async (req, res) => {
+        const mongoClient = new MongoClient(uri);
+        const client = await mongoClient.connect();
+        const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
+        const all = await collection.find({}).toArray();
+        await mongoClient.close();
+        if(all){
+            res.status(200).json({
+                feeders : all
+            });
+        } else {
+            res.status(404);
+        }
     });
-
-    const client = await mongoClient.connect();
-    console.log('client connect');
-    const collection = client.db(process.env.MONGO_DATABASE).collection("feeders");
-    const all = await collection.find({}).toArray();
-    await mongoClient.close();
-    if(all){
-        res.status(200).json({
-            feeders : all
-        });
-    } else {
-        res.status(404);
-    }
-});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
