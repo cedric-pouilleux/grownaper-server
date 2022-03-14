15bf8dfb9354aebdc0e287d6253c7f886147d122
Minart <mino.mnz@gmail.com>
2022-03-14 22:30:41 +0100

    Start implement post add breeders exemple route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAll } from "./routes/breeders";
-
+import {add, getAll} from "./routes/breeders";
 const app = express();
+
 app.use('/breeders', getAll);
+app.use('/breeders', add);
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('server running on port 4000', '');
--- server/routes/breeders.js
+++ server/routes/breeders.js
@@ -4,6 +4,8 @@ import MongodbURI from "../utils/mongodbURI";
 import {Breeders} from "../models";
 const app = express();
 
+app.use(express.json());
+
 export const getAll = app.get('/',
     async (req, res) => {
         await mongoose.connect(MongodbURI);
@@ -14,5 +16,20 @@ export const getAll = app.get('/',
             res.status(404);
         }
         await mongoose.disconnect();
+    }
+);
+
+export const add = app.post('/add',
+    async (req, res) => {
+        const { title, picture, link } = req.body.title;
+        await mongoose.connect(MongodbURI);
+        Breeders.create({ title, picture, link}, async (err, breeder) => {
+            await mongoose.disconnect();
+            if(err){
+                throw new Error('Error with post breeders')
+                res.status(500);
+            }
+            res.status(201).end();
+        });
     }
 );
