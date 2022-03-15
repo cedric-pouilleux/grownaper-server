00495152e836f8b5a7c0d651cde25bed7eca1b32
Minart <mino.mnz@gmail.com>
2022-03-15 02:10:37 +0100

    Rework breeder validaton, add mongoose validator global plugin

--- server/schemas/breeder.js
+++ server/schemas/breeder.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-import uniqueValidator from 'mongoose-unique-validator';
-import slugify from 'slugify-mongoose';
 
 const breederSchema = new Schema({
     title: {
         type: String,
-        required: [true, 'Breed title is required'],
-        minLength: [3, 'Breed title is too short'],
+        required: true,
+        unique: true,
+        minLength: 3,
     },
     slug: {
         type: String,
@@ -17,8 +16,5 @@ const breederSchema = new Schema({
     link: String,
     picture: String,
 });
-
-breederSchema.plugin(uniqueValidator);
-breederSchema.plugin(slugify);
 
 export default breederSchema;
--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,11 @@
+import mongoose from "mongoose";
 import breederSchema from "./breeder";
+
+import uniqueValidator from 'mongoose-unique-validator';
+import slugify from 'slugify-mongoose';
+
+mongoose.plugin(uniqueValidator);
+mongoose.plugin(slugify);
 
 export {
     breederSchema
