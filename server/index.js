341300efd04d0ed6fd41efd6969b9322647d0bbc
Minart <mino.mnz@gmail.com>
2022-03-17 18:18:50 +0100

    Test without varieties and plants routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { breederRoutes, varietyRoutes, plantRoutes } from "./routes";
+import { breederRoutes/*, varietyRoutes, plantRoutes */} from "./routes";
 import cors from 'cors';
 
 const app = express();
@@ -11,13 +11,15 @@ app.use('/breeders', breederRoutes.getAll);
 app.use('/breeders', breederRoutes.postAdd);
 app.use('/breeders', breederRoutes.delete);
 
+/*
+
 app.use('/varieties', varietyRoutes.getAll);
 app.use('/variety', varietyRoutes.postAdd);
 app.use('/variety', varietyRoutes.delete);
 
 app.use('/plants', plantRoutes.getAll);
 app.use('/plant', plantRoutes.postAdd);
-app.use('/plant', plantRoutes.delete);
+app.use('/plant', plantRoutes.delete);*/
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('server running on port 4000', '');
