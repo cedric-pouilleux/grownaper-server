2bd369f1e0276bf5845e8ff68b5e9086c2c1d041
Minart <mino.mnz@gmail.com>
2022-03-17 02:13:11 +0100

    Fix plantRoute appuse

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { breederRoutes, varietyRoutes } from "./routes";
+import { breederRoutes, varietyRoutes, plantRoutes } from "./routes";
 import cors from 'cors';
 
 const app = express();
@@ -15,9 +15,9 @@ app.use('/varieties', varietyRoutes.getAll);
 app.use('/variety', varietyRoutes.postAdd);
 app.use('/variety', varietyRoutes.delete);
 
-app.use('/plants', varietyRoutes.getAll);
-app.use('/plant', varietyRoutes.postAdd);
-app.use('/plant', varietyRoutes.delete);
+app.use('/plants', plantRoutes.getAll);
+app.use('/plant', plantRoutes.postAdd);
+app.use('/plant', plantRoutes.delete);
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('server running on port 4000', '');
